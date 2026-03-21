f302d88aa445bdcbd4f9fed0ad04fce6b6c22801
Jonathan <jonathanleocarle@gmail.com>
2026-03-21 21:47:11 +0100

    update deps, fix linting and formatting (#30)
    * update deps, fix lint and formatting
    
    * add engines field
    
    * added prettierignore and reverted formatting auto-generated files
    
    * review: reformat specifc files

--- ourspace-frontend/src/auth/guard.ts
+++ ourspace-frontend/src/auth/guard.ts
@@ -1,16 +1,16 @@
-import type {RouteLocationNormalized} from "vue-router";
-import {isLoggedIn} from "@/auth/token.ts";
+import type { NavigationGuard } from 'vue-router'
+import { isLoggedIn } from '@/auth/token.ts'
 
-export const authGuard = (to: RouteLocationNormalized, from: RouteLocationNormalized) => {
+export const authGuard: NavigationGuard = (to) => {
   if (isLoggedIn()) {
-    if (to.name === "login") {
-      return {name: "home"};
+    if (to.name === 'login') {
+      return { name: 'home' }
     }
 
-    return;
+    return
   }
 
   if (to.meta.authenticated !== false) {
-    return {name: "login"};
+    return { name: 'login' }
   }
-};
+}
--- ourspace-frontend/src/views/members/components/MemberSelect.vue
+++ ourspace-frontend/src/views/members/components/MemberSelect.vue
@@ -1,23 +1,24 @@
 <script setup lang="ts">
-import {OnyxSelect, OnyxSkeleton, type SelectOption} from "sit-onyx";
-import {computed, ref, watch} from "vue";
-import {type MemberReadable, memberServiceGetMember, memberServiceListMembers} from "@/client";
+import { OnyxSelect, OnyxSkeleton, type SelectOption } from 'sit-onyx'
+import { computed, ref, watch } from 'vue'
+import { type MemberReadable, memberServiceGetMember, memberServiceListMembers } from '@/client'
 
 const props = defineProps<{
-  label: string;
-  isEdit: boolean;
-}>();
-const model = defineModel<string>();
-const selection = ref<MemberReadable>();
-const searchTerm = ref("");
-const response = ref<MemberReadable[]>([]);
+  label: string
+  isEdit: boolean
+}>()
+const model = defineModel<string>()
+const selection = ref<MemberReadable>()
+const searchTerm = ref('')
+const response = ref<MemberReadable[]>([])
 const options = computed<SelectOption<string>[]>(() => {
-  const fromSearch = response.value?.map((member) => ({
-    label: member.name,
-    value: member.id,
-  })) ?? [];
+  const fromSearch =
+    response.value?.map((member) => ({
+      label: member.name,
+      value: member.id,
+    })) ?? []
 
-  const fromSelection = [];
+  const fromSelection = []
   if (selection.value) {
     fromSelection.push({
       label: selection.value.name,
@@ -25,61 +26,75 @@ const options = computed<SelectOption<string>[]>(() => {
     })
   }
 
-  return [...fromSearch, ...fromSelection];
-});
+  return [...fromSearch, ...fromSelection]
+})
 
-watch(model, async () => {
-  if (!model.value) {
-    return;
-  }
-
-  const result = response.value.find((member) => member.id === model.value);
-  if (result) {
-    selection.value = result;
-    return;
-  }
-
-  const member = await memberServiceGetMember({
-    path: {
-      id: model.value,
+watch(
+  model,
+  async () => {
+    if (!model.value) {
+      return
     }
-  });
 
-  if (member.error) {
-    console.error(member.error);
-    return;
-  }
-
-  selection.value = member.data;
-}, {immediate: true});
-
-watch(searchTerm, async () => {
-  const resp = await memberServiceListMembers({
-    query: {
-      name_contains: searchTerm.value,
-      page_size: 20,
+    const result = response.value.find((member) => member.id === model.value)
+    if (result) {
+      selection.value = result
+      return
     }
-  });
 
-  if (resp.error) {
-    console.error(resp.error);
-    return;
-  }
+    const member = await memberServiceGetMember({
+      path: {
+        id: model.value,
+      },
+    })
 
-  response.value = resp.data.members;
-}, {immediate: true});
+    if (member.error) {
+      console.error(member.error)
+      return
+    }
+
+    selection.value = member.data
+  },
+  { immediate: true },
+)
+
+watch(
+  searchTerm,
+  async () => {
+    const resp = await memberServiceListMembers({
+      query: {
+        name_contains: searchTerm.value,
+        page_size: 20,
+      },
+    })
+
+    if (resp.error) {
+      console.error(resp.error)
+      return
+    }
+
+    response.value = resp.data.members
+  },
+  { immediate: true },
+)
 </script>
 
 <template>
-  <OnyxSelect :label="props.label" listLabel="" :options v-model:searchTerm="searchTerm" :withSearch="true" v-model="model" v-if="isEdit"/>
+  <OnyxSelect
+    :label="props.label"
+    listLabel=""
+    :options
+    v-model:searchTerm="searchTerm"
+    :withSearch="true"
+    v-model="model"
+    v-if="isEdit"
+  />
   <div v-else>
-    <div class="onyx-text--small label">{{props.label}}</div>
+    <div class="onyx-text--small label">{{ props.label }}</div>
     <div v-if="selection">
-      {{selection?.name}}
+      {{ selection?.name }}
     </div>
-    <OnyxSkeleton class="selection-skeleton" v-else>
-
-    </OnyxSkeleton>
+    <OnyxSkeleton class="selection-skeleton" v-else> </OnyxSkeleton>
   </div>
 </template>
 
