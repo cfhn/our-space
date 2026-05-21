636b56d7d9306aa08e254b910833f47bcc801b91
natakazie <dev@natakazie.moe>
2026-05-21 22:57:20 +0200

    Created: Presence subpage

---
+++ ourspace-frontend/src/views/presence/PresenceView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { OnyxPageLayout } from 'sit-onyx'
+</script>
+
+<template>
+  <OnyxPageLayout>
+    <h1 class="headline">Presence</h1>
+  </OnyxPageLayout>
+</template>
+
+<style scoped>
+.settings-section {
+  margin-top: var(--onyx-density-lg);
+  margin-bottom: var(--onyx-density-lg);
+}
+</style>
